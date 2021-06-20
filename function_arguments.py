b5839c7388c868f271bd1d2ed7a4747432484bd4
vara0257 <55583893+vara0257@users.noreply.github.com>
2021-06-20 20:07:23 +0530

    Update function_arguments.py

--- function_arguments.py
+++ function_arguments.py
@@ -2,3 +2,12 @@ def my_function(value):
     print(value)
 
 my_function(input("Enter a value:"))
+
+==========================================
+
+def my_function(fname, lname):
+    print ("Full name is", fname, lname)
+
+a, b = input("Enter firstname and lastname:").split()
+my_function(a,b)
+
